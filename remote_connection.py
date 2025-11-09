149b8b0aa86187fe5decc9583c7bbc3b7f67b852
Uduak Akpadok <akpadok1999@gmail.com>
2025-11-09 23:25:02 +0000

    Enhance SSH connection management and validation
    Added SSH connection validation and error handling.

--- remote_connection.py
+++ remote_connection.py
@@ -63,9 +63,45 @@ def sshLogin(ip_addr, username, password, secret):
       'secret' : secret
       } 
 
+      #Calling the connection function
       connection = ConnectHandler(**device_ConfigSSh)
-  except:
-    pass
+
+      #Validation for failed session
+      if sessionSSH is None:
+          print("Failed to establish SSH connection")
+
+      #Enter Privilege enable mode if secret is provided
+      if secret != "":
+          sessionSSH.enable()
+
+      #Retrieving tranpsort input information
+      trans_input_info = sessionSSH.remote_conn.get_transport().sock 
+      remote_ip, remote_port = trans_input_info.getpeername()
+
+      print("\n===============================================")
+      print(f"Connection established through: {blue}{remote_ip}:{remote_port}{reset}")
+      print("SSH Session Closed!")
+      print("===============================================")
+
+      sessionSSH.disconnect()
+
+      return sessionSSH
+      
+  except (RuntimeError, TypeError, NameError, OSError, netmiko.NetMikoTimeoutException) as e:
+      print(f"{red}Error: {e}{reset}")
+
+
+#To validate IP address      
+def validateIP(ip_addr):
+    try:
+        ip = ipaddress.ip_address(ip_addr)
+        return ip,ip.version
+
+    except ValueError as e:
+        print("\n")
+        raise ValueError(f"{e}")
+
+
 
 def main():
     print("\n")
@@ -88,6 +124,8 @@ def main():
                 username = input("Please enter Host Username: ")
                 password = getpass("Please enter Host Password: ")
                 secret = getpass("Please enter the privilege enabled password: ")
+
+                
 
 
             elif option == '2':
